da7685f8014f66a50da6aaa9df71c9cd7c2fb7ba
Veelz <by.veelz@gmail.com>
2025-12-02 20:50:54 -0300

    preparado para render

--- build.sh
+++ build.sh
@@ -10,10 +10,5 @@ python manage.py collectstatic --noinput
 
 # apply migrations
 python manage.py migrate
-# create superuser if not exists
-echo "from django.contrib.auth import get_user_model; User = get_user_model(); \
-if not User.objects.filter(username='admin').exists(): \
-    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')" | python manage.py shell
-# start server
-python manage.py runserver votoautomatizado.onrender.com:8000
+
     
