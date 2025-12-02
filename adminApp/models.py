446557e69547972371f9751ee0460b9b90648ce6
Veelz <by.veelz@gmail.com>
2025-12-02 03:42:12 -0300

    Second commit - cambio al modelo, ya no se guia por base de datos

--- adminApp/models.py
+++ adminApp/models.py
@@ -1,38 +1,13 @@
-# This is an auto-generated Django model module.
-# You'll have to do the following manually to clean this up:
-#   * Rearrange models' order
-#   * Make sure each model has one field with primary_key=True
-#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
-#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
-class Auditoria(models.Model):
-    id_evento = models.AutoField(primary_key=True)
-    fecha_hora_evento = models.DateTimeField()
-    entidad_afectada = models.CharField(max_length=50)
-    id_registro_afectado = models.IntegerField(blank=True, null=True)
-    tipo_evento = models.CharField(max_length=6)
-    usuario_sistema = models.CharField(max_length=50)
-    terminal = models.ForeignKey('Terminal', models.DO_NOTHING, blank=True, null=True)
+class Terminal(models.Model):
+    terminal_id = models.AutoField(primary_key=True)
+    ubicacion = models.CharField(max_length=100, blank=True, null=True)
+    estado_terminal = models.CharField(max_length=14, blank=True, null=True)
 
     class Meta:
-        managed = False
-        db_table = 'auditoria'
-
-
-class CandidatoOpcion(models.Model):
-    candidato_id = models.AutoField(primary_key=True)
-    eleccion = models.ForeignKey('Eleccion', models.DO_NOTHING)
-    nombre_candidato = models.CharField(max_length=100)
-    cargo = models.CharField(max_length=50, blank=True, null=True)
-    partido = models.CharField(max_length=50, blank=True, null=True)
-    lista = models.CharField(max_length=20, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'candidato_opcion'
+        db_table = 'terminal'
 
 
 class Eleccion(models.Model):
@@ -43,43 +18,7 @@ class Eleccion(models.Model):
     estado_eleccion = models.CharField(max_length=9, blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'eleccion'
-
-
-class SesionVotacion(models.Model):
-    id_sesion = models.AutoField(primary_key=True)
-    id_votante = models.ForeignKey('Votante', models.DO_NOTHING, db_column='id_votante')
-    terminal = models.ForeignKey('Terminal', models.DO_NOTHING)
-    fecha_hora_inicio = models.DateTimeField()
-    fecha_hora_fin = models.DateTimeField(blank=True, null=True)
-    estado_sesion = models.CharField(max_length=10, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'sesion_votacion'
-
-
-class Terminal(models.Model):
-    terminal_id = models.AutoField(primary_key=True)
-    ubicacion = models.CharField(max_length=100, blank=True, null=True)
-    estado_terminal = models.CharField(max_length=14, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'terminal'
-
-
-class UsuarioSistema(models.Model):
-    id_usuario = models.AutoField(primary_key=True)
-    username = models.CharField(unique=True, max_length=50)
-    password_hash = models.CharField(max_length=255)
-    rol = models.CharField(max_length=13, blank=True, null=True)
-    id_votante = models.ForeignKey('Votante', models.DO_NOTHING, db_column='id_votante', blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'usuario_sistema'
 
 
 class Votante(models.Model):
@@ -94,13 +33,86 @@ class Votante(models.Model):
     activo = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
         db_table = 'votante'
+
+
+class Auditoria(models.Model):
+    id_evento = models.AutoField(primary_key=True)
+    fecha_hora_evento = models.DateTimeField()
+    entidad_afectada = models.CharField(max_length=50)
+    id_registro_afectado = models.IntegerField(blank=True, null=True)
+    tipo_evento = models.CharField(max_length=6)
+    usuario_sistema = models.CharField(max_length=50)
+    terminal = models.ForeignKey(
+        Terminal, 
+        on_delete=models.SET_NULL,
+        blank=True, 
+        null=True
+    )
+
+    class Meta:
+        db_table = 'auditoria'
+
+
+class SesionVotacion(models.Model):
+    id_sesion = models.AutoField(primary_key=True)
+    id_votante = models.ForeignKey(
+        Votante,
+        on_delete=models.CASCADE,
+        db_column='id_votante'
+    )
+    terminal = models.ForeignKey(
+        Terminal, 
+        on_delete=models.CASCADE
+    )
+    fecha_hora_inicio = models.DateTimeField()
+    fecha_hora_fin = models.DateTimeField(blank=True, null=True)
+    estado_sesion = models.CharField(max_length=10, blank=True, null=True)
+
+    class Meta:
+        db_table = 'sesion_votacion'
+
+
+class CandidatoOpcion(models.Model):
+    class eleccionTipo(models.TextChoices):
+        PRESIDENCIAL = 'Presidencial'
+        PARLAMENTARIA = 'Parlamentaria'
+        MUNICIPAL = 'Municipal'
+    candidato_id = models.AutoField(primary_key=True)
+    eleccion = models.CharField(max_length=13, choices=eleccionTipo.choices)
+    nombre_candidato = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=50, blank=True, null=True)
+    partido = models.CharField(max_length=50, blank=True, null=True)
+    lista = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        db_table = 'candidato_opcion'
+
+
+class UsuarioSistema(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    username = models.CharField(unique=True, max_length=50)
+    password_hash = models.CharField(max_length=255)
+    rol = models.CharField(max_length=13, blank=True, null=True)
+    id_votante = models.ForeignKey(
+        Votante,
+        on_delete=models.SET_NULL,
+        db_column='id_votante',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        db_table = 'usuario_sistema'
 
 
 class Voto(models.Model):
     id_voto = models.AutoField(primary_key=True)
-    id_sesion = models.ForeignKey(SesionVotacion, models.DO_NOTHING, db_column='id_sesion')
+    id_sesion = models.ForeignKey(
+        SesionVotacion,
+        on_delete=models.CASCADE,
+        db_column='id_sesion'
+    )
     tipo_eleccion = models.CharField(max_length=13)
     fecha_hora_emision = models.DateTimeField()
     voto_encriptado = models.TextField()
@@ -108,5 +120,5 @@ class Voto(models.Model):
     comprobante_emision = models.CharField(unique=True, max_length=100)
 
     class Meta:
-        managed = False
         db_table = 'voto'
+
