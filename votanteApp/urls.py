7094b00089018839ddf24a2515334601076f40cd
Veelz <by.veelz@gmail.com>
2025-12-03 02:52:23 -0300

    Desactivacion pdf

--- votanteApp/urls.py
+++ votanteApp/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('panel/', views.panel_votante, name='panel_votante'),
     path('home/', views.votante_home, name='votante_home'),
     path('emitir-voto/', views.emitir_voto, name='emitir_voto'),
-    path("pdf-votos/", views.generar_pdf_votos, name="generar_pdf_votos"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
 
